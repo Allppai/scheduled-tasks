f5e4094871438f57d74cf1bc14cfdbdd3c447678
Allppai <allppai@outlook.com>
2026-03-23 19:58:09 +0100

    Integrate weather API and Twilio for notifications

--- main.py
+++ main.py
@@ -1,38 +1,49 @@
-##################### Extra Hard Starting Project ######################
-
-# 1. Update the birthdays.csv
-
-# 2. Check if today matches a birthday in the birthdays.csv
-
-# 3. If step 2 is true, pick a random letter from letter templates and replace the [NAME] with the person's actual name from birthdays.csv
-
-# 4. Send the letter generated in step 3 to that person's email address.
+import requests
 import os
-import datetime as dt
-import pandas as pd
-import random
-import smtplib
+from twilio.rest import Client
 
-EMAIL = os.environ.get("MY_EMAIL")
-PASSWORD = os.environ.get("MY_PASSWORD")
+timestamp_list = []
 
-now = dt.datetime.now()
-month = now.month
-day = now.day
+MY_LAT = 57.50584
+MY_LONG = -1.79806
+
+# MY_LAT = 53.15270757134602
+# MY_LONG = 18.115509930210457
+
+my_phone = os.environ.get("MY_PHONE")
+twilio_phone = os.environ.get("TWILIO_PHONE")
+
+account_sid = os.environ.get("ACCOUNT_SID")
+auth_token = os.environ.get("AUTH_TOKEN")
+
+OWM_Endpoint = "https://api.openweathermap.org/data/2.5/forecast"
+api_key = os.environ.get("OWM_API_KEY")
+
+parameters = {
+    "lat": MY_LAT,
+    "lon": MY_LONG,
+    "appid": api_key,
+    "cnt": 4
+}
 
 
-data = pd.read_csv("birthdays.csv")
-recipients = [[row["name"],row["email"],row["year"],row["month"],row["day"]] for _, row in data.iterrows()]
+response = requests.get(url=OWM_Endpoint, params=parameters)
+response.raise_for_status()
+weather_data = response.json()
 
-for recipient in recipients:
-    if month in recipient and day in recipient:
-        random_int = f"letter_{random.randint(1,3)}"
-        with open(f"letter_templates/{random_int}.txt","r") as random_letter:
-            default_letter_text = random_letter.read()
-            new_letter_text = default_letter_text.replace("[NAME]", recipient[0])
-            print(new_letter_text)
-            with smtplib.SMTP("smtp.gmail.com", 587) as connection:
-                connection.starttls()
-                connection.login(user=EMAIL, password=PASSWORD)
-                connection.sendmail(from_addr=EMAIL, to_addrs=recipient[1],
-                                    msg=f"Subject:Happy Birthday!\n\n{new_letter_text}")
+for timestamp in weather_data["list"]:
+    print(timestamp["weather"][0]["id"])
+    timestamp_list.append(timestamp["weather"][0]["id"])
+
+if any(weather_id < 700 for weather_id in timestamp_list):
+    client = Client(account_sid, auth_token)
+
+    message = client.messages.create(
+        body="It's going to rain! Take umbrella.",
+        from_=twilio_phone,
+        to=my_phone,
+    )
+    print(message.status)
+
+
+
